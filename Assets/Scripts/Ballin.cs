be4230f6b997108bf0f25a514fc33d012b1c2600
Merby <CynicsMerby@users.noreply.github.com>
2021-02-20 22:29:55 +0300

    Ball Added
    But! We can't hold it, it just rushes to offscreen independently from ballinspeed. I think it doesn't like us :(

---
+++ Assets/Scripts/Ballin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballin : MonoBehaviour
+{
+    Vector2 movement;
+    public float ballinSpeed = 5f;
+    public Rigidbody2D rb;
+    public float direction = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement.x = ballinSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (direction == 1)
+        {
+
+            rb.MovePosition(rb.position + movement * ballinSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position - movement * ballinSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
