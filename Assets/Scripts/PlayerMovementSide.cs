b9896ce03618b0204526e645e264a6c682333e24
Merby <CynicsMerby@users.noreply.github.com>
2021-02-20 21:57:25 +0300

    Minor bug
    When space pressed, character just flies away~~

--- Assets/Scripts/PlayerMovementSide.cs
+++ Assets/Scripts/PlayerMovementSide.cs
@@ -5,6 +5,8 @@ public class PlayerMovementSide : MonoBehaviour
     public float moveSpeed = 5f;
     Vector2 movement;
     public Rigidbody2D rb;
+    public float jumpHeight = 10f;
+    private bool isJumping = false;
 
 
     // Start is called before the first frame update
@@ -17,9 +19,24 @@ public class PlayerMovementSide : MonoBehaviour
     {
 
         movement.x = Input.GetAxisRaw("Horizontal");
+        if(Input.GetKeyDown("space") && !isJumping)
+        {
+            Debug.Log("Space pressed");
+            movement.y += jumpHeight;
+            isJumping = true;
+        }
     }
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
 }
