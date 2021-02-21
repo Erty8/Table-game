9afbd5faa5c1d77c7a072930ad8f10271e2cf799
Merby <CynicsMerby@users.noreply.github.com>
2021-02-21 16:06:25 +0300

    Start Screen Added
    More or Less :P

---
+++ Assets/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
