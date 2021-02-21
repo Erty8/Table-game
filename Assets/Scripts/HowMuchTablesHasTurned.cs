31d7a49fd95a8abda365e7667eb143d3ce0cb451
Merby <CynicsMerby@users.noreply.github.com>
2021-02-21 14:33:31 +0300

    Kaçan Masa eklendi

---
+++ Assets/Scripts/HowMuchTablesHasTurned.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowMuchTablesHasTurned : MonoBehaviour
+{
+    public float turnedTableCount;
+    public float triggerCount = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnedTableCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
