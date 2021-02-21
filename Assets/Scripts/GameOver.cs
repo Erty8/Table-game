8083d65185069100fabe346bf353149b610f406d
Erty8 <41906495+Erty8@users.noreply.github.com>
2021-02-21 18:45:41 +0300

    Scene3

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject overpanel;
+    // Start is called before the first frame update
+    void gameover()
+    {
+        overpanel.gameObject.SetActive(true);
+    }
+    public void overbutton()
+    {
+        Invoke("gameover", 2f);
+    }
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    void SceneChanger()
+    {
+        SceneManager.LoadScene("plat");
+    }
+
+    public void Loadscene()
+    {
+        Invoke("SceneChanger", 2f);
+    }
+}
