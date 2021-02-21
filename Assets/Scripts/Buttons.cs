f7a3f0fe71b9c44149c7ae884f2cbc26d19d232e
Erty8 <41906495+Erty8@users.noreply.github.com>
2021-02-21 19:37:06 +0300

    AlphaRelease

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
@@ -8,5 +9,8 @@ public class Buttons : MonoBehaviour
     {
         Application.Quit();
     }
-
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
 }
